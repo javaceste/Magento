f00644aebd8463af3c1316e199b06bdf82d35726
alexandrucucu <alexandruiuliancucu@yahoo.es>
2018-03-22 09:47:05 +0100

    Model layer

---
+++ Mastering/SampleModule/Model/ResourceModel/Item/Collection.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Mastering\SampleModule\Model\ResourceModel\Item;
+
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+use Mastering\SampleModule\Model\Item;
+use Mastering\SampleModule\Model\ResourceModel\Item as ItemResource;
+
+class Collection extends AbstractCollection
+{
+    protected $_idFieldName = 'id';
+
+    protected function _construct()
+    {
+//		$this->_init(
+//			'Mastering\SampleModule\Model\Item', 
+//			'Mastering\SampleModule\Model\ResourceModel\Item'
+//		);
+        $this->_init(Item::class, ItemResource::class);
+    }
+}
